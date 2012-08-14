ad57149de747509290232fd724cccc6e2f448022
Ben Evans <benjamin.john.evans@gmail.com>
2012-08-14 18:55:45 +0100

    Fix .gitignore

--- src/com/jclarity/anim/memory/MemoryModel.java
+++ src/com/jclarity/anim/memory/MemoryModel.java
@@ -1,36 +1,36 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.jclarity.anim.memory;
 
 
 /**
- * This class implements 
+ * This class implements a simple memory model, with a fixed number of rows
+ * but variable number of columns
  * 
  * @author boxcat
  */
 public class MemoryModel {
 
-    //FIXME Remove all this? Only the component rendering the information should
-    //care about anything to do with heights
-//    private final int height;
-//    private final int width;
-//    private final int wEden;
-//    private final int wSrv;
-//
-//    private final MemoryBlock[][] eden;
-//    private final MemoryBlock[][] s1;
-//    private final MemoryBlock[][] s2;
-//    private final MemoryBlock[][] tenured;
-//    
-//    private int tenuringThreshold = 4;
+    private final int height = 8;
+    private final int wEden;
+    private final int wSrv;
+    private final int wOld;
+
     
-    public MemoryModel() {
-//        eden = new MemoryBlock[wEden][height];
-//        s1 = new MemoryBlock[wSrv][height];
-//        s2 = new MemoryBlock[wSrv][height];
-//        tenured = new MemoryBlock[width - (wEden + wSrv)][height];
+    private final MemoryBlock[][] eden;
+    private final MemoryBlock[][] s1;
+    private final MemoryBlock[][] s2;
+    private final MemoryBlock[][] tenured;
+    
+    private int tenuringThreshold = 4;
+    
+    public MemoryModel(int wEden_, int wSrv_, int wOld_) {
+        wEden = wEden_;
+        wSrv = wSrv_;
+        wOld = wOld_;
+        
+        eden = new MemoryBlock[wEden][height];
+        s1 = new MemoryBlock[wSrv][height];
+        s2 = new MemoryBlock[wSrv][height];
+        tenured = new MemoryBlock[wOld][height];
     }
 
     
