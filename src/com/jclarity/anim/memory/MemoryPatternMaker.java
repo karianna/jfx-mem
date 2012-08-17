0c387bea6acff0a526d227812a9862b6a9dfdf49
Ben Evans <benjamin.john.evans@gmail.com>
2012-08-17 17:56:34 +0100

    Pattern maker

---
+++ src/com/jclarity/anim/memory/MemoryPatternMaker.java
@@ -0,0 +1,32 @@
+package com.jclarity.anim.memory;
+
+/**
+ * The point of this class is to allocate a predictable amount of memory.
+ * We should allocate twice as much as we free.
+ * 
+ * So the class should return ALLOC ALLOC FREE (repeat)
+ * 
+ */
+class MemoryPatternMaker implements IMemoryInterpreter {
+
+    private volatile int seq = 0;
+    
+    private volatile int alloc_seq = 0;
+    
+    private volatile int free_seq = 0;
+    
+    public MemoryPatternMaker() {
+    }
+
+    @Override
+    public MemoryInstruction getNextStep() {
+        seq++;
+        if (seq % 3 ==0 ) {
+            return new MemoryInstruction(OpCode.KILL, free_seq++);
+        } else {
+            return new MemoryInstruction(OpCode.ALLOC, alloc_seq++);
+        }
+        
+    }
+
+}
