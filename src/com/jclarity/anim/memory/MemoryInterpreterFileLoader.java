bd8147c1beea76dd3ef43f7e9339aaf03f3d754f
Ben Evans <benjamin.john.evans@gmail.com>
2012-09-11 19:04:25 +0200

    And this one

--- src/com/jclarity/anim/memory/MemoryInterpreterFileLoader.java
+++ src/com/jclarity/anim/memory/MemoryInterpreterFileLoader.java
@@ -1,12 +1,32 @@
 package com.jclarity.anim.memory;
 
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Iterator;
+import java.util.List;
+
+import static com.jclarity.anim.memory.OpCode.*;
+
 /**
  *
  * @author jpgough
  */
 public class MemoryInterpreterFileLoader implements IMemoryInterpreter {
-    
+
     private final String filePath;
+    private List<String> lines = null;
+    private Iterator<String> it = null;
+    
+    private enum Command {
+        
+    }
+    
+    private final static String nopStr = "NOP\\s+\\d+";
+    private final static String allocStr = "ALLOC";
+    private final static String largeAllocStr = "LARGE_ALLOC";
+
     
     public MemoryInterpreterFileLoader(String filePath) {
         //Potentially load the file here rather than holding the string
@@ -15,14 +35,29 @@ public class MemoryInterpreterFileLoader implements IMemoryInterpreter {
 
     @Override
     public MemoryInstruction getNextStep() {
-        //1. Process next step in the file
-        
-        //2. Add to the model
-        
-        //Model will do the validation of the add and move the memory around
-        //From generation to generation as appropriate
-        
-        return null;
+        if (lines == null) {
+            loadLines();
+        }
+
+        // Process next line in the file
+        String line = it.next();
+        if (line == null) {
+            return new MemoryInstruction(EOF);
+        }
+
+        return parseLine(line);
     }
-    
+
+    private void loadLines() {
+        try {
+            lines = Files.readAllLines(Paths.get(filePath), Charset.defaultCharset());
+            it = lines.iterator();
+        } catch (IOException ex) {
+            throw new RuntimeException("File: " + filePath + " not found");
+        }
+    }
+
+    private MemoryInstruction parseLine(String line) {
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
 }
